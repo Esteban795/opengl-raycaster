3a8d6db6d3a0d7d79ffabc703fa14afbd836fd0d
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2023-06-23 18:54:36 +0200

    shader code

---
+++ include/shader.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+
+
+struct Shader {
+    GLuint program;
+};
+
+typedef struct Shader* Shader;
+
+bool shader_init(Shader* shader, const char* vertex_source, const char* fragment_source);
+
+void shader_destroy(Shader shader);
+
+void shader_use(Shader shader);
---
+++ src/shader.c
@@ -0,0 +1,36 @@
+#include "../include/shader.h"
+
+
+
+bool shader_init(Shader* shader, const char* vertex_source, const char* fragment_source) {
+    *shader = malloc(sizeof(struct Shader));
+
+    //creating vertex shader
+    unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_source, NULL);
+    glCompileShader(vertex_shader);
+
+    //creating fragment shader
+    unsigned int fragment_shader;
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_source, NULL);
+    glCompileShader(fragment_shader);
+
+
+    (*shader)->program = glCreateProgram();
+    //linking shaders to the program
+    glAttachShader((*shader)->program, vertex_shader);
+    glAttachShader((*shader)->program, fragment_shader);
+    glLinkProgram((*shader)->program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+}
+
+void shader_destroy(Shader shader) {
+    glDeleteProgram(shader->program);
+}
+
+void shader_use(Shader shader) {
+    glUseProgram(shader->program);
+}
