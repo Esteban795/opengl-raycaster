04490dc63bfe74009d348d44d04e5b8a8725bf09
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2023-06-29 14:09:51 +0200

    finally works !

--- src/ray.c
+++ src/ray.c
@@ -1,113 +1,141 @@
 #include "../include/ray.h"
 
-int get_ray_color(char symbol){
-    switch (symbol){
-    case 'r':
-        return 0xFF2222;
-    case 'g':
-        return 0x22FF22;
-    case 'b':
-        return 0x2222FF;
-    case '#':
-        return 0x111111;
-    case 'w':
-        return 0xEEEEEE;
-    case 'a':
-        return 0x22EEEE;
-    default:
-        return 0xF1F1F1;
+unsigned int get_ray_color(unsigned char symbol) {
+    switch(symbol) {
+        case 'r':
+            return 0xFF2222;
+        case 'g':
+            return 0x22FF22;
+        case 'b':
+            return 0x2222FF;
+        default:
+            return 0x222222;
     }
 }
 
-double get_incidence_angle(enum Side side,double angle){
-    switch (side)
-    {
-    case WEST:
-        return M_PI_2 - (angle - M_PI - M_PI_2);
-    case EAST:
-        return M_PI_2 - (angle - M_PI_2);
-    case NORTH:
-        return M_PI_2 - angle;
-    case SOUTH:
-        return M_PI_2 - (angle - M_PI);
-    default:
-        return 0;
+double get_incidence_angle(enum Side side, double angle) {
+
+    switch(side) {
+        case WEST:
+            return M_PI_2 - (angle - M_PI - M_PI_2);
+        case EAST:
+            return M_PI_2 - (angle - M_PI_2);
+        case NORTH:
+            return M_PI_2 - angle;
+        case SOUTH:
+            return M_PI_2 - (angle - M_PI);
+        default:
+            return 0;
     }
 }
 
-enum Side get_side_hit(int x, int y, int prev_x,int prev_y){
-    if (x > prev_x) return WEST;
-    if (x < prev_x) return EAST;
-    if (y > prev_y) return NORTH;
-    if (y < prev_y) return SOUTH;
-    return WEST;
+enum Side get_side_hit(int x, int y, int prev_x, int prev_y) {
+
+    if (x > prev_x) {
+        return WEST;
+    }
+    if (x < prev_x) {
+        return EAST;
+    }
+    if (y > prev_y) {
+        return NORTH;
+    }
+    if (y < prev_y) {
+        return SOUTH;
+    }
 }
 
-double get_ray_depth(position player_pos,int map_x, int map_y, double dx, double dy, double scale, enum Side side){
-    if (dx == 0){
-        int add_height = dy < 0 ? 1 : 0; //when casting in negative directions, you need to account for the width of map cells
-        return fabs((map_y + add_height) * scale - player_pos.y);
+double get_ray_depth(position player_position, int map_x, int map_y, double delta_x, double delta_y, double scale, enum Side side) {
+
+    if (delta_x == 0) {
+
+        int add_height = (delta_y < 0) ? 1 : 0; // When casting in negative directions, you need to account for the width of map cells
+        return fabs((map_y + add_height)*scale - player_position.y);
     }
-    if (dy == 0){
-        int add_width = dx < 0 ? 1 : 0;
-        return fabs((map_x + add_width) * scale - player_pos.x);
+
+    if (delta_y == 0) {
+
+        int add_width = (delta_x < 0) ? 1 : 0;
+        return fabs((map_x + add_width)*scale - player_position.x);
     }
-    double x,y;
-    if (side == NORTH || side == SOUTH){
-        double slope = dy/dx;
-        int add_height = side == SOUTH ? 1 : 0;
-        y = (map_y + add_height) * scale;
-        x = y/slope + player_pos.x - player_pos.y/slope;
-    } else {
-        double slope = dx/dy;
-        int add_width = side == EAST ? 1 : 0;
-        x = (map_x + add_width) * scale;
-        y = x/slope + player_pos.y - player_pos.x/slope;
+
+    if (side == NORTH || side == SOUTH) {
+
+        double slope = delta_y/delta_x;
+        int add_height = (side == SOUTH) ? 1 : 0;
+
+        double y = (map_y + add_height) * scale;
+        double x = y/slope + player_position.x - player_position.y/slope;
+
+        return sqrt((player_position.x - x)*(player_position.x - x) + (player_position.y - y)*(player_position.y - y));
     }
-    return sqrt((player_pos.x - x) * (player_pos.x - x) + (player_pos.y - y) * (player_pos.y - y));
+
+    if (side == EAST || side == WEST) {
+
+        double slope = delta_x/delta_y;
+        int add_width = (side == EAST) ? 1 : 0;
+
+        double x = (map_x + add_width) * scale;
+        double y = x/slope + player_position.y - player_position.x/slope;
+
+        return sqrt((player_position.x - x)*(player_position.x - x) + (player_position.y - y)*(player_position.y - y));
+    }
 }
 
+ray cast_ray(map m, position position, double ray_angle, double player_angle) {
 
-void print_ray(ray r){
-    printf("Ray : (%f; %u, %f)\n\n",r.depth,r.color,r.angle_of_incidence);
-}
-
-ray cast_ray(map m,position pos,double ray_angle,double player_angle){
     ray r;
-    int map_x = (int) (pos.x / m->scale);
-    int map_y = (int) (pos.y / m->scale);
-    int prev_map_x,prev_map_y;
-    double march_x = 0.0;
-    double march_y = 0.0;
-    double detail = 10;
-    double dx = -sin(ray_angle)/detail;
-    double dy = cos(ray_angle)/detail;
-    char curr; //what's the letter
+
+    int map_x = (int) (position.x/ m->scale);
+    int map_y = (int) (position.y/ m->scale);
+
+    int prev_map_x;
+    int prev_map_y;
+
+    double march_x = 0;
+    double march_y = 0;
+
+    double detail = 10; // This will determine how small the steps are when doing a raycast
+    // Detail values too low will cause corners of cells be be lost due to resampling
+
+    double delta_x = -sin(ray_angle)/detail;
+    double delta_y = cos(ray_angle)/detail;
+
+    char curr;
 
     do {
-        march_x += dx;
-        march_y += dy;
+        march_x += delta_x;
+        march_y += delta_y;
+
         prev_map_x = map_x;
         prev_map_y = map_y;
-        map_x = (int) ((pos.x + march_x) / m->scale);
-        map_y = (int) ((pos.y + march_y) / m->scale);
-        if (map_x < 0 || map_x >= m->width || map_y < 0 || map_y > m->height){ // outside of the map
-            r.depth = 10000000;
+
+        map_x = (int) ((position.x+march_x)/m->scale);
+        map_y = (int) ((position.y+march_y)/m->scale);
+
+        if (map_x < 0 || map_x >= m->width || map_y < 0 || map_y >= m->height) {
+
+            r.depth = 1000000;
             r.color = 0x000000;
+            
             return r;
         }
-        curr = m->world[map_y * m->width + map_x];
-    } while (curr == ' ' || curr == 'p');
-    if ( map_x != prev_map_x && map_y != prev_map_y) {
-        //weird case where the ray goes perfectly through a corner so ray hit isn't registered..
-        //we just cast another ray with a very small different angle
-        return cast_ray(m,pos,ray_angle - 0.001,player_angle);
+
+        curr = m->world[map_y*m->width + map_x];
+
+    } while (curr == ' ' || curr == 'P');
+
+    if ((map_x != prev_map_x) && (map_y != prev_map_y)) {
+        // There are cases where the ray passes perfectly through a corner and the side of the block that was hit cannot be known
+        // In these cases, do the ray cast in a very slightly different direction
+        return cast_ray(m, position, ray_angle - 0.0001, player_angle);
+    } else {
+        enum Side side = get_side_hit(map_x, map_y, prev_map_x, prev_map_y);
+        double raw_depth = get_ray_depth(position, map_x, map_y, delta_x, delta_y, m->scale, side)/m->scale;
+        r.depth = fabs(cos(player_angle - ray_angle)*raw_depth); // Find the distance to the plane of the player rather than to the player
+        r.color = get_ray_color(curr);
+        r.angle_of_incidence = get_incidence_angle(side, ray_angle);
     }
-    enum Side side = get_side_hit(map_x,map_y,prev_map_x,prev_map_y);
-    double depth = get_ray_depth(pos,map_x,map_y,dx,dy,m->scale,side) / m->scale;
-    r.depth = fabs(cos(player_angle - ray_angle)) * depth;//fabs(cos(player_angle - ray_angle) / depth); //find distance to the plane of the player rather than to the player directly to avoid fisheye effect
-    r.color = get_ray_color(curr);
-    r.angle_of_incidence = get_incidence_angle(side,ray_angle);
-    //print_ray(r);
+
     return r;
 }
--- src/raycaster.c
+++ src/raycaster.c
@@ -4,58 +4,19 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-// Glew must come before opengl else it won't work.
+// Glew must come before opengl
 #include <GL/glew.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
 
 #include "../include/window.h"
 #include "../include/shader.h"
 #include "../include/map.h"
 #include "../include/ray.h"
 
-#define WIDTH 1024
-#define HEIGHT 768  
+#define WIDTH 800
+#define HEIGHT 600
 #define NUM_THREADS 4
-
-const float quad_vertex_vata[] = { 
-    // pos      // tex
-    0.0f, 1.0f, 0.0f, 1.0f,
-    1.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 0.0f, 
-
-    0.0f, 1.0f, 0.0f, 1.0f,
-    1.0f, 1.0f, 1.0f, 1.0f,
-    1.0f, 0.0f, 1.0f, 0.0f
-};
-
-window win = NULL;
-Shader shader = NULL;
-map m;
-position player_pos;
-SDL_Event event;
-
-const double world_scale = 10;
-double player_angle = 0;
-const double player_speed = 70;
-const double mouse_sensitivity = 20;
-const double fov = 100;
-double half_fov;
-double focus_to_image;
-const unsigned int light_color = 0xFFFFFF;
-char texture_data[WIDTH * HEIGHT * 3];
-GLuint vao = -1;
-GLuint screen_texture = -1;
-
-bool quit = false;
-bool keydown_z = false;
-bool keydown_q = false;
-bool keydown_s = false;
-bool keydown_d = false;
-bool keydown_left = false;
-bool keydown_right = false;
-
-int mouse_move_x = 0;
 
 const char* vertex_shader_source = "#version 300 es\n"
     "precision highp float;\n"
@@ -76,174 +37,21 @@ const char* fragment_shader_source = "#version 300 es\n"
         // "color = vec4(1.0f*TexCoords.x, 0.6f*TexCoords.y, 1.0f, 1.0f);\n"
     "}\0";
 
-void create_screen_texture(void){
-    glGenTextures(1, &screen_texture);
-    glBindTexture(GL_TEXTURE_2D, screen_texture);
-    
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-    glGenerateMipmap(GL_TEXTURE_2D);
+const float quadVertexData[] = { 
+    // pos      // tex
+    0.0f, 1.0f, 0.0f, 1.0f,
+    1.0f, 0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
+    0.0f, 1.0f, 0.0f, 1.0f,
+    1.0f, 1.0f, 1.0f, 1.0f,
+    1.0f, 0.0f, 1.0f, 0.0f
+};
 
-    glBindTexture(GL_TEXTURE_2D, 0); 
-}
+pthread_t threads[NUM_THREADS];
+int thread_args[NUM_THREADS];
 
-void create_quad_VAO() {
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    
-    // The verticies will never change so the buffer ID is not saved
-    GLuint vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertex_vata),quad_vertex_vata, GL_STATIC_DRAW);
-    
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    // Free bound buffers
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-}
-
-void* render_thread(void* thread_num) {
-    float thread_div = (float) WIDTH / NUM_THREADS;
-    int thread_start = thread_div * *((int*)thread_num);
-    int thread_end = thread_div * (*((int*)thread_num) + 1);
-
-    for (int x = thread_start; x < thread_end; x++) {
-        ray ray = cast_ray(m, player_pos, player_angle + atan((x - (WIDTH / 2)) / focus_to_image), player_angle);
-        int wall_height = (int) HEIGHT / ray.depth;
-        printf("Ray depth : %f\n wall_height : %d\n\n",ray.depth,wall_height);
-        for (int y = 0; y < HEIGHT; y++) {
-            if (y > (HEIGHT - wall_height) / 2 && y < wall_height + (HEIGHT - wall_height) / 2) {
-                texture_data[(y * WIDTH + x) * 3] = ray.color >> 16;
-                texture_data[(y * WIDTH + x) * 3 + 1] = ray.color >> 8;
-                texture_data[(y * WIDTH + x) * 3 + 2] = ray.color >> 0;
-            }
-        }
-    }
-    pthread_exit(0);
-}
-
-void render(pthread_t* threads,int* thread_args) {
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_create(&(threads[i]), NULL, render_thread, (void*) (&(thread_args[i])));
-    }
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    glBindTexture(GL_TEXTURE_2D, screen_texture); 
-    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, WIDTH, HEIGHT,GL_RGB, GL_UNSIGNED_BYTE,texture_data);
-    glClear(GL_COLOR_BUFFER_BIT);
-    shader_use(shader);
-    glBindVertexArray(vao);
-    glBindTexture(GL_TEXTURE_2D, screen_texture);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
-    glBindVertexArray(0);
-    glUseProgram(0);
-    window_swap(win);
-    glBindTexture(GL_TEXTURE_2D, 0); 
-}
-
-void updatePlayer(uint64_t delta) {
-    // Mouse movement
-    player_angle += mouse_move_x / 1000.0 * mouse_sensitivity;
-    // Arrow movement
-    if (keydown_left != keydown_right) {
-        float change = (keydown_left) ? -1 : 1;
-        float arrow_speed = 3.5f;
-        player_angle += (delta / 1000.0) * change * arrow_speed;
-    }
-    float x_fraction;
-    float y_fraction;
-    float mult = (delta/1000.0) * player_speed;
-    if ((keydown_z != keydown_s)) {
-        mult = keydown_s ? mult*-1 : mult;
-        x_fraction = -sin(player_angle);
-        y_fraction = cos(player_angle);
-        player_pos.x += x_fraction * mult;
-        player_pos.y += y_fraction * mult;
-    }
-    if ((keydown_q != keydown_d)) {
-        mult = keydown_s ? -mult : mult;
-        float turn_angle = keydown_d ? M_PI_2 : -M_PI_2;
-        x_fraction = -sin(player_angle + turn_angle);
-        y_fraction = cos(player_angle + turn_angle);
-        player_pos.x += x_fraction *mult;
-        player_pos.y += y_fraction * mult;
-    }
-}
-
-void pollEvents() {
-    while (SDL_PollEvent(&event)) {
-        switch (event.type) {
-            case SDL_QUIT:
-                quit = true;
-                break;
-            case SDL_WINDOWEVENT:
-                if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-                    glViewport(0, 0, event.window.data1, event.window.data2);
-                }
-                break;
-            case SDL_MOUSEMOTION:
-                mouse_move_x = event.motion.xrel;
-                break;
-            case SDL_KEYDOWN:
-                switch(event.key.keysym.sym) {
-                    case SDLK_ESCAPE:
-                        quit = true;
-                        break;
-                    case SDLK_z:
-                        keydown_z = true;
-                        break;
-                    case SDLK_q:
-                        keydown_q = true;
-                        break;
-                    case SDLK_s:
-                        keydown_s = true;
-                        break;
-                    case SDLK_d:
-                        keydown_d = true;
-                        break;
-                    case SDLK_LEFT:
-                        keydown_left = true;
-                        break;
-                    case SDLK_RIGHT:
-                        keydown_right = true;
-                        break;
-                }
-                break;
-            case SDL_KEYUP:
-                switch(event.key.keysym.sym) {
-                    case SDLK_z:
-                        keydown_z = false;
-                        break;
-                    case SDLK_q:
-                        keydown_q = false;
-                        break;
-                    case SDLK_s:
-                        keydown_s = false;
-                        break;
-                    case SDLK_d:
-                        keydown_d = false;
-                        break;
-                    case SDLK_LEFT:
-                        keydown_left = false;
-                        break;
-                    case SDLK_RIGHT:
-                        keydown_right = false;
-                        break;
-                }
-                break;
-            default:
-                // Ignore other events
-                break;
-        }
-    }
-}
-
+map m;
 char world[] = 
     "rrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
     "b                 g          g"
@@ -267,50 +75,340 @@ char world[] =
 
 const int world_width = 30;
 const int world_height = 19;
+const double world_scale = 10;
 
-int main(void){
-    //int world_width,world_height;
-    //char* world = read_world(argv[2]);
+position player_position;
+double player_angle = 0;
+const double player_speed = 70;
+const double mouse_sensitivity = 20;
+
+const double fov = 100;
+double half_fov;
+double focus_to_image;
+
+const double max_fog_distance = 20;
+const double min_fog_distance = 2;
+const unsigned int fog_color = 0x87CEEB;
+const unsigned int light_color = 0xFFFFFF;
+
+window win = NULL;
+Shader shader = NULL;
+char texture_data[WIDTH*HEIGHT*3];
+GLuint vao = -1;
+GLuint screen_texture = -1;
+
+bool quit = false;
+SDL_Event event;
+
+bool keydown_w = false;
+bool keydown_a = false;
+bool keydown_s = false;
+bool keydown_d = false;
+bool keydown_left = false;
+bool keydown_right = false;
+
+int mouse_move_x = 0;
+
+void createScreenTexture() {
+
+    glGenTextures(1, &screen_texture);
+    glBindTexture(GL_TEXTURE_2D, screen_texture);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); 
+
+    glBindTexture(GL_TEXTURE_2D, 0); 
+}
+
+void createQuadVAO() {
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    // The verticies will never change so the buffer ID is not saved
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertexData), quadVertexData, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    // Free bound buffers
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+}
+
+unsigned int lerpColor(unsigned int color_1, unsigned int color_2, double lin_val) {
+
+    unsigned int newColor = 0x000000;
+    unsigned int mask = 0xFF;
+
+    newColor |= (unsigned char) (((float)((color_1 >> 16)&mask) * (1-lin_val)) + ((float)((color_2 >> 16)&mask) * lin_val));
+    newColor <<= 8;
+    newColor |= (unsigned char) (((float)((color_1 >> 8)&mask) * (1-lin_val)) + ((float)((color_2 >> 8)&mask) * lin_val));
+    newColor <<= 8;
+    newColor |= (unsigned char) (((float)((color_1 >> 0)&mask) * (1-lin_val)) + ((float)((color_2 >> 0)&mask) * lin_val));
+
+    return newColor;
+}
+
+double getFogAmount(double depth) {
+
+    return (depth > min_fog_distance) ? fmin((depth-min_fog_distance)/(max_fog_distance-min_fog_distance), 0.8) : 0;
+}
+
+void* renderScene(void* thread_num) {
+
+    float thread_div = (float) WIDTH / NUM_THREADS;
+
+    int thread_start = thread_div * *((int*)thread_num);
+    int thread_end = thread_div * (*((int*)thread_num) + 1);
+
+    for (int x = thread_start; x < thread_end; x++) {
+
+        ray r = cast_ray(m, player_position, player_angle + atan((x-(WIDTH/2))/focus_to_image), player_angle);
+        int wall_height = (int) (( HEIGHT / (r.depth)));
+
+        for (int y = 0; y < HEIGHT; y++) {
+
+            if (y > (HEIGHT-wall_height)/2 && y < wall_height+(HEIGHT-wall_height)/2) {
+
+                texture_data[(y*WIDTH + x)*3 + 0] = r.color >> 16;
+                texture_data[(y*WIDTH + x)*3 + 1] = r.color >> 8;
+                texture_data[(y*WIDTH + x)*3 + 2] = r.color >> 0;
+
+            } else {
+                texture_data[(y*WIDTH + x)*3 + 0] = 0;
+                texture_data[(y*WIDTH + x)*3 + 1] = 0;
+                texture_data[(y*WIDTH + x)*3 + 2] = 0;
+            }
+        }
+    }
+
+    pthread_exit(0);
+}
+
+void render() {
+
+    for (int it = 0; it < NUM_THREADS; it++) {
+        pthread_create(&(threads[it]), NULL, renderScene, (void*) (&(thread_args[it])));
+    }
+
+    for (int it = 0; it < NUM_THREADS; it++) {
+        pthread_join(threads[it], NULL);
+    }
+
+    glBindTexture(GL_TEXTURE_2D, screen_texture); 
+    
+    glTexSubImage2D(
+        GL_TEXTURE_2D, 
+        0, 
+        0, 
+        0, 
+        WIDTH, 
+        HEIGHT,
+        GL_RGB, 
+        GL_UNSIGNED_BYTE,
+        texture_data
+    );
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    shader_use(shader);
+
+    glBindVertexArray(vao);
+    glBindTexture(GL_TEXTURE_2D, screen_texture);
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glBindVertexArray(0);
+    glUseProgram(0);
+    window_swap(win);
+
+    glBindTexture(GL_TEXTURE_2D, 0); 
+}
+
+void updatePlayer(uint64_t delta) {
+
+    // Mouse movement
+    player_angle += mouse_move_x/1000.0 * mouse_sensitivity;
+    
+    // Arrow movement
+    if (keydown_left != keydown_right) {
+
+        float change = (keydown_left) ? -1 : 1;
+        float arrow_speed = 3.5f;
+        player_angle += (delta/1000.0)*change*arrow_speed;
+    }
+
+    float x_fraction;
+    float y_fraction;
+
+    float mult = (delta/1000.0)*player_speed;
+
+    if ((keydown_w != keydown_s)) {
+
+        mult = keydown_s ? mult*-1 : mult;
+
+        x_fraction = -sin(player_angle);
+        y_fraction = cos(player_angle);
+
+        player_position.x += x_fraction*mult;
+        player_position.y += y_fraction*mult;
+    }
+
+    if ((keydown_a != keydown_d)) {
+
+        mult = keydown_s ? mult*-1 : mult;
+
+        float turn_angle = keydown_d ? M_PI_2 : -M_PI_2;
+
+        x_fraction = -sin(player_angle + turn_angle);
+        y_fraction = cos(player_angle + turn_angle);
+
+        player_position.x += x_fraction*mult;
+        player_position.y += y_fraction*mult;
+    }
+}
+
+void pollEvents() {
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                // User clicked the close button, exit loop
+                printf("Quit event received\n");
+                quit = true;
+                break;
+
+            case SDL_WINDOWEVENT:
+
+                if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                    glViewport(0, 0, event.window.data1, event.window.data2);
+                }
+                break;
+            case SDL_MOUSEMOTION:
+
+                mouse_move_x = event.motion.xrel;
+                break;
+
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym) {
+
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+
+                    case SDLK_w:
+                        keydown_w = true;
+                        break;
+
+                    case SDLK_a:
+                        keydown_a = true;
+                        break;
+
+                    case SDLK_s:
+                        keydown_s = true;
+                        break;
+
+                    case SDLK_d:
+                        keydown_d = true;
+                        break;
+                    
+                    case SDLK_LEFT:
+                        keydown_left = true;
+                        break;
+                    
+                    case SDLK_RIGHT:
+                        keydown_right = true;
+                        break;
+                }
+                break;
+
+            case SDL_KEYUP:
+                switch(event.key.keysym.sym) {
+
+                    case SDLK_w:
+                        keydown_w = false;
+                        break;
+
+                    case SDLK_a:
+                        keydown_a = false;
+                        break;
+
+                    case SDLK_s:
+                        keydown_s = false;
+                        break;
+
+                    case SDLK_d:
+                        keydown_d = false;
+                        break;
+
+                    case SDLK_LEFT:
+                        keydown_left = false;
+                        break;
+                    
+                    case SDLK_RIGHT:
+                        keydown_right = false;
+                        break;
+                }
+                break;
+
+            default:
+                // Ignore other events
+                break;
+        }
+    }
+}
+
+int main(void) {
+
     win = window_create(WIDTH, HEIGHT);
+
     m = create_map(world, world_width, world_height, world_scale);
-    player_pos = get_player_pos(m);
-    int mod_30 = 0;
-    // thread args
-    pthread_t threads[NUM_THREADS];
-    int thread_args[NUM_THREADS];
+    player_position = get_player_pos(m);
+
+    // Prepare thread args
     for (int it = 0; it < NUM_THREADS; it++) {
         thread_args[it] = it;
     }
 
     if (!window_init(win)) {
+
         printf("Failed to initialize SDL\n");
-        return 0;
 
     } else {
 
         printf("SDL initialized\n");
 
         shader_init(&shader, vertex_shader_source, fragment_shader_source);
-        printf("Shaders compiled successfully.\n");
-        create_screen_texture();
+        createScreenTexture();
 
         glClearColor(0.5, 0.2, 0.5, 1.0);
-        create_quad_VAO();
+        createQuadVAO();
 
-        half_fov = 0.5 * (fov / 180.0f * M_PI);
-        focus_to_image = (WIDTH / 2) / tan(half_fov);
+        half_fov = (fov/ 180.0f * M_PI)/2.0f;
+        focus_to_image = (WIDTH/2)/tan(half_fov);
 
-        int last_frame = SDL_GetTicks();
-        int current_frame = SDL_GetTicks();
-        int delta_time = 0;
+        uint64_t last_frame = SDL_GetTicks64();
+        uint64_t current_frame = SDL_GetTicks64();
+        uint64_t delta_time = 0;
+
         while (!quit) {
-            current_frame = SDL_GetTicks();
+
+            current_frame = SDL_GetTicks64();
             delta_time = current_frame - last_frame;
             last_frame = current_frame;
+
             // Poll for events
             pollEvents();
             updatePlayer(delta_time);
-            render(threads,thread_args);
+
+            render();
+            // printf("%f %f %f\n", player_position.x, player_position.y, player_angle);
+
             mouse_move_x = 0;
         }
     }  
